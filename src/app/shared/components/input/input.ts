895225e977701e162ba1f6752bce259d0727e46d
pamela lotriet <pamelalotriet@gmail.com>
2026-07-01 09:50:25 +0200

    reusable input text created with reactive forms

--- src/app/shared/components/input/input.ts
+++ src/app/shared/components/input/input.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule,ReactiveFormsModule, FormsModule],
   templateUrl: './input.html',
   styleUrl: './input.css',
 })
-export class Input {}
+export class Input {
+  control = input<FormControl>( new FormControl(''));
+  floatLabel = input<string>('Enter text');
+  errorMessage = input<string>('');
+}
