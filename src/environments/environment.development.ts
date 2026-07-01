cde7436c687cf88e5fdaf01c70dcad732113ac69
pamela lotriet <pamelalotriet@gmail.com>
2026-07-01 15:38:34 +0200

    Added firebase configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBts0h7bYd9Bj7xc2WPhzg-3ZMBXHuVk6E',
+    authDomain: 'the-archive-f2cfd.firebaseapp.com',
+    projectId: 'the-archive-f2cfd',
+    storageBucket: 'the-archive-f2cfd.firebasestorage.app',
+    messagingSenderId: '870041772181',
+    appId: '1:870041772181:web:e8a52c4412ea5faf853b9c',
+    measurementId: 'G-NZPREJLEEX',
+  },
+};
