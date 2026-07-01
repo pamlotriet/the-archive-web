376f32081f5a47f400556d7c0d20751bf1224a5b
pamela lotriet <pamelalotriet@gmail.com>
2026-07-01 15:27:36 +0200

    added translations to component

--- src/app/shared/components/input/input.ts
+++ src/app/shared/components/input/input.ts
@@ -1,16 +1,32 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+export type InputType = 'text' | 'email' | 'password' | 'number' | 'tel' | 'url' | 'search';
+
 @Component({
   selector: 'app-input',
-  imports: [InputTextModule, FloatLabelModule, ReactiveFormsModule, FormsModule],
+  imports: [InputTextModule, FloatLabelModule, ReactiveFormsModule, FormsModule, TranslatePipe],
   templateUrl: './input.html',
   styleUrl: './input.css',
 })
-export class Input {
+export class InputComponent {
   control = input<FormControl>(new FormControl(''));
-  floatLabel = input<string>('Enter text');
+  floatLabel = input<string>('common.input.defaultLabel');
   errorMessage = input<string>('');
   customClass = input<string>('');
+  type = input<InputType>('text');
+  placeholder = input<string>('');
+
+  protected readonly passwordVisible = signal(false);
+
+  protected displayedType(): InputType {
+    return this.type() === 'password' && this.passwordVisible() ? 'text' : this.type();
+  }
+
+  protected togglePasswordVisibility(): void {
+    this.passwordVisible.update((visible) => !visible);
+  }
 }
