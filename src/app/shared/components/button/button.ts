fdf643b1bc9800f9287b2f76179002e24355eb20
pamela lotriet <pamelalotriet@gmail.com>
2026-07-01 10:10:53 +0200

    created reusable button

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ButtonDirective } from 'primeng/button';
+import { ButtonSeverity, ButtonVariant } from './types/button.types';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [ButtonDirective],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
-export class Button {}
+export class Button {
+  buttonText = input<string>('Button');
+  iconName = input<string>('home');
+  iconPosition = input<'left' | 'right'>('left');
+  iconOnly = input<boolean>(false);
+  showIcon = input<boolean>(false);
+  severity = input<ButtonSeverity>('primary');
+  variant = input<ButtonVariant>();
+  rounded = input<boolean>(false);
+}
---
+++ src/app/shared/components/button/types/button.types.ts
@@ -0,0 +1,4 @@
+export type ButtonSeverity =
+  'primary' | 'secondary' | 'success' | 'info' | 'warn' | 'help' | 'danger' | 'contrast';
+
+export type ButtonVariant = 'outlined' | 'text' | 'link';
