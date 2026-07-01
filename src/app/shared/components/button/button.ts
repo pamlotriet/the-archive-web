8e8dad231a7aa96167d3c23fce9a780b9c03a512
pamela lotriet <pamelalotriet@gmail.com>
2026-07-01 15:27:32 +0200

    added translations to component

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,15 +1,16 @@
 import { Component, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { ButtonDirective } from 'primeng/button';
-import { ButtonSeverity, ButtonVariant } from './types/button.types';
+import { ButtonSeverity, ButtonVariant } from '@shared/components/button/types/button.types';
 
 @Component({
   selector: 'app-button',
-  imports: [ButtonDirective],
+  imports: [ButtonDirective, TranslatePipe],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
-export class Button {
-  buttonText = input<string>('Button');
+export class ButtonComponent {
+  buttonText = input<string>('common.button.defaultLabel');
   iconName = input<string>('home');
   iconPosition = input<'left' | 'right'>('left');
   iconOnly = input<boolean>(false);
