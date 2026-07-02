41e91c57ab90c2aa9a6e7c57b452d96ba0ef9e95
pamela lotriet <pamelalotriet@gmail.com>
2026-07-02 09:05:17 +0200

    registered store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 
 import { routes } from '@app/app.routes';
+import {
+  AuthenticationApiEffects,
+  authenticationApiFeatureKey,
+  authenticationApiReducer,
+} from '@features/authentication/state/authentication-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideRouter(routes),
+    provideStore(),
+    provideState(authenticationApiFeatureKey, authenticationApiReducer),
+    provideEffects(AuthenticationApiEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+    }),
     provideTranslateService({
       fallbackLang: 'en',
       lang: 'en',
