aeb45b4c8cdc6b7cbd99ca600f1a6e5c5a840c45
pamela lotriet <pamelalotriet@gmail.com>
2026-07-02 09:04:04 +0200

    Create models needed for authentication state

--- src/app/features/authentication/models/auth.models.ts
+++ src/app/features/authentication/models/auth.models.ts
@@ -1,1 +1,12 @@
-export interface AuthenticationState {}
+import { AppUser } from './user.models';
+
+export interface AuthenticationApiState {
+  user: AppUser | null;
+  loading: boolean;
+  error: string | null;
+}
+
+export type UserCredentials = {
+  email: string;
+  password: string;
+};
---
+++ src/app/features/authentication/models/user.models.ts
@@ -0,0 +1,7 @@
+// app-user.model.ts
+export type AppUser = {
+  uid: string;
+  email: string | null;
+  displayName: string | null;
+  photoUrl: string | null;
+}
