b473049ece45185243240f81e63e342d8f1c2ea9
pamela lotriet <pamelalotriet@gmail.com>
2026-07-02 09:48:31 +0200

    Added dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
@@ -6,5 +7,13 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () =>
       import('@features/authentication/pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('@features/dashboard/pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+    canActivate: [AuthGuard],
   },
 ];
