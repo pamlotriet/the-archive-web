6ae22291e76d00701f287c472759f1cc7752bcde
pamela lotriet <pamelalotriet@gmail.com>
2026-07-03 10:14:49 +0200

    added notes page

---
+++ src/app/features/notes/pages/notes/notes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes',
+  imports: [],
+  templateUrl: './notes.component.html',
+  styleUrl: './notes.component.css',
+})
+export class NotesComponent {}
