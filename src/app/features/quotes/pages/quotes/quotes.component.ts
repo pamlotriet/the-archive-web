85ea9f1952b8605dbc1b29b02ff73c46c9ba1e75
pamela lotriet <pamelalotriet@gmail.com>
2026-07-03 10:14:56 +0200

    added quotes page

---
+++ src/app/features/quotes/pages/quotes/quotes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes',
+  imports: [],
+  templateUrl: './quotes.component.html',
+  styleUrl: './quotes.component.css',
+})
+export class QuotesComponent {}
